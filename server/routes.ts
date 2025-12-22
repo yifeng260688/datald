ff4d0773c53e6afa3767623dfde450a5f0469a33
yifeng260688 <yifeng260688@gmail.com>
2025-12-23 04:48:53 +0700

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -86,7 +86,7 @@ const userStorageMulter = multer.diskStorage({
 const upload = multer({
   storage: userStorageMulter,
   limits: {
-    fileSize: 500 * 1024 * 1024, // 500MB max
+    fileSize: 1024 * 1024 * 1024, // 1GB max
   },
   fileFilter: (_req, file, cb) => {
     const allowedTypes = [
